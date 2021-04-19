aea1e87446c1838cd0c3fb4aab0fccb264c39ded
Emmanuel <makwale.em@gmail.com>
2021-04-19 15:54:23 +0200

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer(){
+    let date = new Date();
+    return (
+        <h3>This is footer </h3>
+    )
+}
+
+export default Footer;
---
+++ src/components/MainContents.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MainContencts(){
+    return (
+        <div className="main-contents">
+            <h1>Wellome to my first React app</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            <a href="#">Read More...</a>
+        </div>
+    )
+}
+
+export default MainContencts;
